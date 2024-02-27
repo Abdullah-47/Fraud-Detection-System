91c11b3f38b56791509ac101a5f1e64187369390
Nilesh Gawli <nbgawli@gmail.com>
2024-02-27 16:26:23 +0530

    feat: Add Login Screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import react from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Screens/Home";
+import Login from "./Components/Screens/Login";
+import "./App.css";
 
 function App() {
-
   return (
     <>
+    <Login/>
+    {/* <BrowserRouter>
+      <Routes>
 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
+    </BrowserRouter> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content:["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        secondary: '#96EF1D',
+        dark: '#0C0E12',
+        light: 'white',
+        lightGray: '#a0a0a0'
+      },
+      fontFamily: {
+        Onest: ["Onest", "sans-serif"],
+      },
+      backgroundImage: {
+        'pattern': "url('/src/assets/Image/login_image.jpg')",
+      },
+    },
   },
   plugins: [],
 }
