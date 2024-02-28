38e0583536f7695ede6973c63cc86ff9368b7368
Nilesh Gawli <nbgawli@gmail.com>
2024-02-28 08:06:06 +0530

    chores: fix styles

--- src/Components/Screens/Home.jsx
+++ src/Components/Screens/Home.jsx
@@ -48,7 +48,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="h-screen w-full bg-[#fff5e9]  flex justify-center items-center text-center font-Onest">
+      <div className="h-screen w-full bg-[#fff5e9] opacity-80 flex justify-center items-center text-center font-Onest">
         <div className="text-6xl w-[80%] text-[#120E05]  tracking-wider">
           We're a group of passionate individuals working hard to keep your
           financial transactions safe. Using simple yet effective technology, we
@@ -73,14 +73,14 @@ export default function Home() {
 const Navbar = () => {
   return (
     <div className="w-full flex justify-between items-center font-Onest p-6 fixed ">
-      <h1 className="text-5xl font-bold text-white border-b-2 border-white cursor-pointer hover:text-[#84721A]">
+      <h1 className="text-5xl font-bold text-white border-b-2 border-white cursor-pointer hover:text-[#f6e271]">
         Fraud <br /> Detection
       </h1>
       <div className="flex flex-col md:flex-row justify-between items-center md:space-x-4 text-xl font-semibold text-white self-start ">
-        <a className="text-white hover:text-[#84721A]" href="/login">
+        <a className="text-white hover:text-[#f6e271]" href="/login">
           Login
         </a>
-        <a className="text-white hover:text-[#84721A]" href="/signup">
+        <a className="text-white hover:text-[#f6e271]" href="/signup">
           Signup
         </a>
       </div>
